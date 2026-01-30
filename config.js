f75aa1705be5c6a6903efe6f00268b04190645ce
S.S. Syntax-Corp <asgu1520@gmail.com>
2026-01-31 01:41:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  MAX_COMMANDS_PER_SERVER: 100,
-  MAX_COMMAND_LENGTH: 20000,
-  RATE_LIMIT: {
-    USER: { MAX: 10, WINDOW: 60000 },
-    GUILD: { MAX: 100, WINDOW: 60000 }
-  }
+    TOKEN: process.env.TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    // REPLACE the URL below with your actual Railway Domain from Step 4
+    REDIRECT_URI: "https://your-railway-domain.up.railway.app/callback.html" 
 };
